7f9076edb08d79bc4536a139ee98ce29008bc1ef
Erhan Karayiğit <erhan.karayigit@gmail.com>
2023-08-11 00:03:57 +0300

    Add files via upload

--- handlers/middlewares/auth/middleware.go
+++ handlers/middlewares/auth/middleware.go
@@ -12,6 +12,11 @@ func Inıt(next http.Handler) http.Handler {
 			http.Error(w, "Access is denied", http.StatusUnauthorized)
 			return
 		}
+
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
 		next.ServeHTTP(w, r)
 	})
 }
